aee7865843867d0703fc9dd84070c741526250f9
rubens Rodrigues <rubens@schoolguardian.app>
2022-03-26 11:22:09 -0300

    Aula 3 - Parte 1

--- src/Screens/Home/HomeController.js
+++ src/Screens/Home/HomeController.js
@@ -1,30 +1,63 @@
-import React from 'react';
+import React from 'react'
 import HomeView from './HomeView'
-import HomeModel from './HomeModel'
 
 class HomeController extends React.Component {
 
-    constructor() {
-        super();
-        this.homeModel = new HomeModel();//inicializando o view    
-        this.homeModel.getSomeInfo();
-        this.state = {
-            count: 0
-        };
+    constructor(props) {
+        super(props);
 
-        // setInterval(() => {
-        //     this.setState({
-        //         count: this.state.count + 1
-        //     })
-        // }, 1000);
+        //gerando logs da props
+        console.log(" Chamando constructor() ");
+        console.log(props);
+
+        this.state = {
+            count: 0,
+        }; //inicializando o state
+
+        //Inicializando o timeout
+        this.interval = setInterval(() => {
+            //atualizando o contador
+            console.log(" Atualizando a classe ");
+            this.setState({
+                count: this.state.count + 1
+            })
+        }, 3000);
     }
 
+    componentDidMount() {
+        console.log(" Chamando componentDidMount ");
+    }
 
+    shouldComponentUpdate(nextProps, nextState) {
+        //Só irá atualizar quando o count for diferente de 1
+        if (this.state.count === 1) {
+            console.log(" Chamando shouldComponentUpdate = false ");
+            return false;
+        } else {
+            console.log(" Chamando shouldComponentUpdate = true ");
+            return true;
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log(" Chamando componentDidUpdate ");
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval)
+        console.log(" Chamando componentWillUnmount ");
+    }
 
     render() {
+        console.log(" Chamando Render " + this.state.count);
         return (
-            <HomeView count={this.state.count} /> //Chamando o View
+            //Chamando o View e passando o props count_info
+            <HomeView
+                count={this.state.count}
+            />
         )
     }
+
+
 }
 export default HomeController;
