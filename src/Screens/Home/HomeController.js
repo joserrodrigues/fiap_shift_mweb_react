021576821669790971150369855696968957ca36
rubens Rodrigues <rubens@schoolguardian.app>
2022-03-26 13:22:16 -0300

    Aula 5 - Parte 3

--- src/Screens/Home/HomeController.js
+++ src/Screens/Home/HomeController.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useContext } from 'react';
+import React, { useState, useEffect, useRef, useContext } from 'react';
 import useAPI from '../../Services/APIs/Common/useAPI';
 import toys from '../../Services/APIs/Toys/toys';
 import HomeView from './HomeView';
@@ -9,9 +9,11 @@ import { InfoContext } from "../../store/InfoContext";
 const HomeController = ({ coords, isGeolocationAvailable, isGeolocationEnabled }) => {
 
     const getToysGetAPI = useAPI(toys.getAllToys);
+    const getToysPaginateAPI = useAPI(toys.getToysPaginate);
     const navigate = useNavigate();
     const userCoordinates = useRef(null);
     const context = useContext(InfoContext);
+    const [viewType, setViewType] = useState("cards");
 
     console.log(context);
 
@@ -24,6 +26,50 @@ const HomeController = ({ coords, isGeolocationAvailable, isGeolocationEnabled }
     useEffect(() => {
         getToysGetAPI.request(1);
     }, []);
+
+    const getDataPage = (query) => {
+        return new Promise((resolve, reject) => {
+            console.log(query);
+
+            let page = query.page + 1
+            let info = `page=${page}&perPage=${query.pageSize}`;
+            if (query.orderBy !== undefined && query.orderBy !== "") {
+                info += `&orderBy=${query.orderBy.field}`
+            }
+            if (query.orderDirection !== undefined && query.orderDirection !== "") {
+                info += `&orderDirection=${query.orderDirection}`
+            }
+            if (query.search !== undefined && query.search !== "") {
+                info += `&search=${query.search}`
+            }
+            getToysPaginateAPI.requestPromise(info, context.tokenLogin)
+                .then(info => {
+                    console.log(info);
+                    resolve({
+                        data: info.toys,
+                        page: info.page - 1,
+                        totalCount: info.totalItems
+                    });
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        })
+    }
+
+    const onChangeViewType = (viewTypeInfo) => {
+        console.log(viewTypeInfo);
+        setViewType(viewTypeInfo);
+    }
+
+    const addToy = () => {
+        navigate("/detail/add", {
+            state: {
+                latitude: userCoordinates.current ? userCoordinates.current.latitude : 0,
+                longitude: userCoordinates.current ? userCoordinates.current.longitude : 0
+            }
+        })
+    }
 
     console.log(userCoordinates);
     const goToPage = (toy) => {
@@ -38,7 +84,10 @@ const HomeController = ({ coords, isGeolocationAvailable, isGeolocationEnabled }
     }
 
     console.log(getToysGetAPI.data)
-    return <HomeView arrayToys={getToysGetAPI.data} loading={getToysGetAPI.loading} goToPage={goToPage} info={context.info} />
+    return <HomeView arrayToys={getToysGetAPI.data} loading={getToysGetAPI.loading}
+        goToPage={goToPage} info={context.info} getDataPage={getDataPage}
+        viewType={viewType} onChangeViewType={onChangeViewType}
+        addToy={addToy} />
 }
 export default geolocated({
     positionOptions: {
--- src/Screens/Home/HomeView.js
+++ src/Screens/Home/HomeView.js
@@ -1,66 +1,41 @@
 import * as React from 'react';
 import './Home.css'
-import { Box, Container, CircularProgress, Grid, Typography, Button, Stack } from '@mui/material';
-import { Card, CardContent, CardMedia } from '@mui/material';
-import CardInfo from '../../Components/CardInfo/CardInfo'
+import { Box, Container, Grid, Typography } from '@mui/material';
 import Header from '../../Components/Header/Header';
 
-export default function HomeView({ loading, arrayToys, goToPage, info }) {
+import ToysTableView from '../../Components/ToysTableView/ToysTableView';
+import ToysCardsView from '../../Components/ToysCardsView/ToysCardsView';
+import ToysTopList from '../../Components/ToysTopList/ToysTopList';
+import renderIf from 'render-if';
 
-    let infoBox = [];
+export default function HomeView({ loading, arrayToys, goToPage, info, getDataPage, viewType, onChangeViewType, addToy }) {
 
-    if (loading) {
-        infoBox.push(
-            <Grid key={1} item lg={12} xl={12} className="itemClass">
-                <CircularProgress />
-            </Grid>
-
-        )
-    } else if (arrayToys) {
-        arrayToys.toys.forEach(toy => {
-            infoBox.push(
-                <Grid key={toy._id} item sm={12} md={6} lg={2} xl={2} className="itemClass" onClick={() => goToPage(toy)}>
-                    <Card className='cardClass'>
-                        <CardMedia
-                            component="img"
-                            height="140"
-                            src={toy.mainImage}
-                            alt={toy.name}
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="body2" component="div" className='titleCard'>
-                                {toy.name}
-                            </Typography>
-                            <CardInfo />
-                        </CardContent>
-                    </Card>
-                </Grid>
-            )
-        });
-    }
     return (
         <Container fixed className="container" maxWidth="lg">
-            <Stack>
-                <Header />
-                <Box className="contentBox">
-                    <Grid
-                        container
-                        spacing={3}
-                        alignItems="center"
-                    >
-                        <Grid item lg={6} xl={6} className="titlePage">
-                            <Typography variant="h1" >
-                                Base de Brinquedos - {info}
-                            </Typography>
-                        </Grid>
-                        <Grid item lg={6} xl={6} className="titleButton">
-                            <Button variant='primary' className='buttonClass'>Cadastrar brinquedo</Button>
-                        </Grid>
-                        {infoBox}
-
+            <Header />
+            <Box className="contentBox">
+                <Grid
+                    container
+                    spacing={3}
+                    alignItems="center"
+                >
+                    <Grid item lg={12} xl={12} className="titlePage">
+                        <Typography variant="h1" >
+                            Base de Brinquedos - {info}
+                        </Typography>
                     </Grid>
-                </Box>
-            </Stack>
+                    <ToysTopList onChangeView={onChangeViewType} viewType={viewType} addToy={addToy} />
+
+                    <Grid item lg={12} xl={12} >
+                        {renderIf(viewType === "cards")(
+                            <ToysCardsView loading={loading} arrayToys={arrayToys} goToPage={goToPage} />
+                        )}
+                        {renderIf(viewType === "table")(
+                            <ToysTableView loading={loading} goToPage={goToPage} getDataPage={getDataPage} />
+                        )}
+                    </Grid>
+                </Grid>
+            </Box>
         </Container>
     );
 }
