4294a2498d61cb257d353fab52ec6673a5cc3a86
rubens Rodrigues <rubens@schoolguardian.app>
2022-03-26 09:43:46 -0300

    Aula 1 - Parte 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 
 function App() {
+  const testeJSX = <div className="App"> Outro Codigo </div>
+
   return (
-    <div className="App">
-      <p>
-        Hello World!!!
-      </p>
+    <div className="container">
+      <div className="App">Hello World
+      </div>
+      {testeJSX}
     </div>
   );
 }
